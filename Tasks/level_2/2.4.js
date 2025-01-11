e492f50a759c5bcc19b0a116d226f28675b4364a
p0lluxstar <web-registra@yandex.ru>
2025-01-11 18:18:24 +0300

    fix: all 1-20

--- Tasks/level_2/2.4.js
+++ Tasks/level_2/2.4.js
@@ -17,6 +17,8 @@ function fun(num) {
 
 fun(1000000000); // Вызов функции
 
+/********************************************/
+
 // 2. Сделайте функцию, которая параметром будет принимать число и проверять, простое оно или нет.
 
 function isPrime(num) {
@@ -39,3 +41,34 @@ console.log(isPrime(10)); // false (составное)
 console.log(isPrime(17)); // true (простое)
 console.log(isPrime(1));  // false (1 не является простым)
 console.log(isPrime(0));  // false (0 не является простым)
+
+/********************************************/
+
+// 3. Сделайте функцию, которая параметром будет принимать число и удалять из него четные цифры.
+
+function fun(num){
+    return Number(String(num).split('').filter( el => Number(el) % 2 !== 0).join(''))
+    
+  }
+  
+  fun(14567432465)
+
+/********************************************/
+
+// 4. Сделайте функцию, которая заполнит массив N случайными числами из заданного промежутка.
+
+function fillArrayWithRandomNumbers(n, min, max) {
+    const result = [];
+
+    for (let i = 0; i < n; i++) {
+        // Генерация случайного числа в диапазоне [min, max]
+        const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+        result.push(randomNum);
+    }
+
+    return result;
+}
+
+// Пример использования:
+console.log(fillArrayWithRandomNumbers(5, 1, 100)); // Например: [45, 23, 67, 89, 10]
+console.log(fillArrayWithRandomNumbers(10, -50, 50)); // Например: [-20, 15, 0, -3, 49, ...]
